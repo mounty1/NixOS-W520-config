fb991158c5074bdb5a3ee1673b5481b437b7ffb7
Michael Mounteney <github@landcroft.com>
2023-05-13 06:31:55 +1000

    Experimental Nouveau implementation

--- nvidia.nix
+++ nvidia.nix
@@ -1,25 +1,5 @@
 { config, pkgs, lib, ... }:
 
 {
-	services.xserver.videoDrivers = [ "nvidia" ];
-
-
-	# For nvidia
-	nixpkgs.config.allowUnfree = true;
-
-	hardware.nvidia = {
-		# powerManagement.enable = true;
-		package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
-		modesetting.enable = true;
-		prime = {
-			# offload.enable = true;
-			sync.enable = true;
-
-			# Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-			nvidiaBusId = "PCI:1:0:0";
-
-			# Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-			intelBusId = "PCI:0:2:0";
-		};
-	};
+	services.xserver.videoDrivers = [ "nouveau" ];
 }
